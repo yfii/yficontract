6a2e094003a6e1dfae27205f10838b11f3bea296
jin10086 <igaojin@qq.com>
2020-07-25 16:50:25 +0800

    init

---
+++ getsol.py
@@ -0,0 +1,25 @@
+import requests
+
+s = requests.Session()
+
+
+def getsourcecode(contract_address):
+    url = f"http://api.etherscan.io/api?module=contract&action=getsourcecode&address={contract_address}&apikey=AWXR3HCJBWXTXAG61BYD43CVUGC2MDA5UG"
+    z1 = s.get(url)
+    return z1.json()["result"][0]["SourceCode"]
+
+
+contracts = {
+    "yCurveFiRewardsAddress": "0x0001FB050Fe7312791bF6475b96569D83F695C9f",
+    "balancerRewardsAddress": "0x033E52f513F9B98e129381c6708F9faA2DEE5db5",
+    "yfiAddress": "0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e",
+    "claimAddress": "0xcc9EFea3ac5Df6AD6A656235Ef955fBfEF65B862",
+    "governanceAddress": "0x3A22dF48d84957F907e67F4313E3D43179040d6E",
+    "bpoolAddress": "0x95c4b6c7cff608c0ca048df8b81a484aa377172b",
+    "feeRewardsAddress": "0xb01419E74D8a2abb1bbAD82925b19c36C191A701",
+}
+
+for name, address in contracts.items():
+    source_code = getsourcecode(address)
+    with open(f"{name}.sol", "w") as f:
+        f.write(source_code)
